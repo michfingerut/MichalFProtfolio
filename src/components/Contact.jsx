c583917ab98e8b6ea752390a76b723f3d4481768
michal.fingerut <michal.fingerut@infinitylabs.co.il>
2025-04-16 11:29:34 +0300

    remove redundent rows

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,17 +19,12 @@ function ContactPage() {
         I specialize in web development, building scalable applications, and
         staying up-to-date with the latest in technology.
         <br />
-        I'm excited to contribute fresh ideas and deliver high-quality
-        solutions.
-        <br />
         You can contact me directly through the icons below via WhatsApp or
         other platforms.
       </p>
       <ContactInfo>
         <strong>Phone:</strong> 054-2398171
         <br />
-        <strong>Email:</strong>{' '}
-        <a href="mailto:michfingerut@gmail.com">michfingerut@gmail.com</a>
       </ContactInfo>
 
       <a href="./Michal_Fingerut.pdf" download="Michal_Fingerut_CV">
