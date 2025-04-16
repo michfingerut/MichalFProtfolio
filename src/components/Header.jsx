81b7bf9726fc3d7949227282aeddb5e78513c851
michal.fingerut <michal.fingerut@infinitylabs.co.il>
2025-04-16 14:57:36 +0300

    move about intro text to header and move nav bar from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,75 +1,21 @@
-// External modules
-import { Link } from 'react-scroll';
-
 // Internal modules
-import { NavStyle, NavContainer } from '../styles/navbarStyles';
 import { HeaderContainer } from '../styles/headerStyles';
-import { theme } from '../styles/theme';
 import { SecondaryContainer } from '../styles/commonsStyles';
-
-//TODO: split to sub components?
-const linkStyle = {
-  color: theme.primaryText,
-  margin: '0 25px',
-  fontSize: '18px',
-  fontWeight: '600',
-  textTransform: 'uppercase',
-  letterSpacing: '2px',
-  position: 'relative',
-  padding: '5px 0',
-  cursor: 'pointer',
-  transition: 'color 0.3s ease, transform 0.3s ease',
-};
-
-const onOver = (e) => {
-  e.target.style.color = theme.hoverColor;
-  e.target.style.transform = 'scale(1.1)';
-};
-
-const onOut = (e) => {
-  e.target.style.color = theme.primaryText;
-  e.target.style.transform = 'scale(1)';
-};
 
 function Header() {
   return (
     <HeaderContainer id="header-container">
       <SecondaryContainer>
-        <NavContainer>
-          <NavStyle>
-            <Link
-              to="about-section"
-              smooth={true}
-              style={linkStyle}
-              onMouseOver={(e) => onOver(e)}
-              onMouseOut={(e) => onOut(e)}
-            >
-              About
-            </Link>
-            <Link
-              to="projects-section"
-              smooth={true}
-              style={linkStyle}
-              onMouseOver={(e) => onOver(e)}
-              onMouseOut={(e) => onOut(e)}
-            >
-              Projects
-            </Link>
-
-            <Link
-              to="contact-section"
-              smooth={true}
-              style={linkStyle}
-              onMouseOver={(e) => onOver(e)}
-              onMouseOut={(e) => onOut(e)}
-            >
-              Contact
-            </Link>
-          </NavStyle>
-        </NavContainer>
         <h1>Michal Fingerut</h1>
-        <p>A Software Developer based in Central district, Israel.</p>
-        <p>Looking for the next challenge!</p>
+        <h2>A Software Developer based in Central district, Israel.</h2>
+        <p>
+          Successfully transitioned from a career in optometry to software
+          development,
+          <br />
+          showcasing adaptability, a strong learning mindset, and a commitment
+          to continuous growth.
+        </p>
+        <h2>Looking for the next challenge!</h2>
       </SecondaryContainer>
     </HeaderContainer>
   );
